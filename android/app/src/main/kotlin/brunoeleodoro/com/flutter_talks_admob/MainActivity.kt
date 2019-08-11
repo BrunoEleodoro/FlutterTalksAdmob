1f8452f4b19ef08cfadb76718e5b68eaf6e5b26a
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2019-08-11 11:55:05 -0300

    First commit

---
+++ android/app/src/main/kotlin/brunoeleodoro/com/flutter_talks_admob/MainActivity.kt
@@ -0,0 +1,13 @@
+package brunoeleodoro.com.flutter_talks_admob
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
