fc4659d9339e33e429765e5d7ba312b2a71730ab
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2019-08-31 12:50:39 -0300

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -35,68 +35,17 @@ class _MyHomePageState extends State<MyHomePage> {
     keywords: <String>['flutterio', 'beautiful apps'],
     contentUrl: 'https://flutter.io',
     childDirected: false,
-    designedForFamilies: false,
-    gender: MobileAdGender.male,
     testDevices: <String>[],
   );
-
-  BannerAd myBanner;
-  InterstitialAd myInterstitial;
-  int clicks = 0;
-  void startBanner() {
-    myBanner = BannerAd(
-      adUnitId: BannerAd.testAdUnitId,
-      size: AdSize.smartBanner,
-      targetingInfo: targetingInfo,
-      listener: (MobileAdEvent event) {
-        print("BannerAd event is $event");
-      },
-    );
-  }
-
-  void displayBanner() {
-    myBanner
-      ..load()
-      ..show(
-        anchorOffset: 0.0,
-        anchorType: AnchorType.bottom,
-      );
-  }
-
-  @override
-  void dispose() {
-    myBanner?.dispose();
-    myInterstitial?.dispose();
-    super.dispose();
-  }
 
   @override
   void initState() {
     super.initState();
     FirebaseAdMob.instance
         .initialize(appId: "ca-app-pub-3940256099942544~3347511713");
-
-    startBanner();
-    displayBanner();
-  }
-
-  InterstitialAd buildInterstitial() {
-    return InterstitialAd(
-        adUnitId: InterstitialAd.testAdUnitId,
-        targetingInfo: MobileAdTargetingInfo(testDevices: <String>[]),
-        listener: (MobileAdEvent event) {
-          if (event == MobileAdEvent.loaded) {
-            myInterstitial?.show();
-          }
-          if (event == MobileAdEvent.clicked || event == MobileAdEvent.closed) {
-            myInterstitial.dispose();
-            clicks = 0;
-          }
-        });
   }
 
   void onTap(position) {
-    shouldDisplayTheAd();
     if (position == 0) {
       Navigator.pushNamed(context, '/first');
     } else {
@@ -104,44 +53,32 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  void shouldDisplayTheAd() {
-    clicks++;
-    if (clicks >= 3) {
-      myInterstitial = buildInterstitial()
-        ..load()
-        ..show();
-      clicks = 0;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.only(bottom: 50),
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Home Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text(
+            'Home Page',
+            style: TextStyle(fontSize: 25),
           ),
-          body: Container(
-            child: Center(
-              child: Text(
-                'Home Page',
-                style: TextStyle(fontSize: 25),
-              ),
-            ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: this.onTap,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_alarm),
+            title: Text('First Screen'),
           ),
-          bottomNavigationBar: BottomNavigationBar(
-            onTap: this.onTap,
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.access_alarm),
-                title: Text('First Screen'),
-              ),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.account_balance_wallet),
-                  title: Text('Second Screen')),
-            ],
-          ),
-        ));
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_balance_wallet),
+              title: Text('Second Screen')),
+        ],
+      ),
+    );
   }
 }
